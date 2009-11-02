76e00bee11f20836747cff8501c7808680a8b50c
Will Nowak <compbrain@gmail.com>
2009-11-02 16:30:16 -0500

    Add 5s caching for generated json

--- printqueue/viewer/views.py
+++ printqueue/viewer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.conf import settings
+from django.views.decorators.cache import cache_page
 
 import printqueue
 printqueue.MEMCACHE_HOST = settings.MEMCACHE_HOST
@@ -22,7 +23,7 @@ def QueueView(self, name, template_name='viewer/queueview.html'):
   p = printqueue.PrintQueue(name)
   return render_to_response(template_name, {'queuename':name, 'queue':p})
 
-
+@cache_page(5)
 def JSONQueueView(self, name):
   name = GetBaseQueueName(name)
   p = printqueue.PrintQueue(name)
